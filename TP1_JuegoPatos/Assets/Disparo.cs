64d8b4f1025346dee90826752d97ff9743628485
xaviersaez <126728159+xaviersaez@users.noreply.github.com>
2023-11-08 20:24:34 -0300

    codigo del disparo, falta definir lo que destruye

--- TP1_JuegoPatos/Assets/Disparo.cs
+++ TP1_JuegoPatos/Assets/Disparo.cs
@@ -13,6 +13,27 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Se ha hecho clic izquierdo
+            Vector3 mousePosition = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // El clic ha golpeado un objeto
+                GameObject objetoGolpeado = hit.collider.gameObject;
+                if (objetoGolpeado.CompareTag("Objetivo"))
+                {
+                    // Destruye el objeto
+                    Destroy(objetoGolpeado);
+                    Debug.Log("Clic izquierdo destruyó el objeto: " + objetoGolpeado.name);
+                }
+                   
+            }
+        }
     }
 }
+    
