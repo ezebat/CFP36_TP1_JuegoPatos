cc62ffe6c0559bc6db9a5b126187e7e7eee92298
ezebat <150198473+ezebat@users.noreply.github.com>
2023-12-01 18:07:25 -0300

    saco el getcomponent

--- TP1_JuegoPatos/Assets/Apuntar.cs
+++ TP1_JuegoPatos/Assets/Apuntar.cs
@@ -10,16 +10,17 @@ public class Apuntar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        apuntocuerpo.GetComponent<Renderer>().transform.Rotate(0, 0, 0);
-        apuntocamara.GetComponent<Renderer>().transform.Rotate(0, 0, 0);
+        apuntocuerpo.transform.Rotate(0, 0, 0);
+        apuntocamara.transform.Rotate(0, 0, 0);
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //transform.Rotate(speed * Time.deltaTime * Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X") * speed * Time.deltaTime, 0);
-        apuntocuerpo.GetComponent<Renderer>().transform.Rotate(0, Input.GetAxis("Mouse X") * speed * Time.deltaTime, 0);
-        apuntocamara.GetComponent<Renderer>().transform.Rotate(speed * Time.deltaTime * Input.GetAxis("Mouse Y"), 0, 0);
+        apuntocuerpo.transform.Rotate(0, Input.GetAxis("Mouse X") * speed * Time.deltaTime, 0);
+        apuntocamara.transform.Rotate(speed * Time.deltaTime * Input.GetAxis("Mouse Y"), 0, 0);
 
     }
 }
