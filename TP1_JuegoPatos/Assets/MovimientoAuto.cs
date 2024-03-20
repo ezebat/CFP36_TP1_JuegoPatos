9a9c27a41aab182c4b607d737bb6c968ae1a9005
xaviersaez <126728159+xaviersaez@users.noreply.github.com>
2024-03-20 20:03:00 -0300

    movimientos del auto

--- TP1_JuegoPatos/Assets/MovimientoAuto.cs
+++ TP1_JuegoPatos/Assets/MovimientoAuto.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
 
 public class MovimientoAuto : MonoBehaviour
 {
 
     public int movilidad = 1;
+    
+    public GameObject auto;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,15 @@ public class MovimientoAuto : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, movilidad * Time.deltaTime, 0);
+        transform.Translate(0, 0, movilidad * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(movilidad *-1 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(movilidad * Time.deltaTime, 0, 0);
+        }
     }
 }
